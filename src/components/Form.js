02e621f298f0c67f4c762c7def0eec9105f5ef90
Danil Khasanov <65128636+SenpaiBabai@users.noreply.github.com>
2022-09-02 17:41:08 +0500

    Update Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,39 +1,38 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 
-function Form(props) {
-    const  [name, setName] = useState('')
+function Form(props){
+    const [nameInput, setNameInput] = useState('')
 
-    function handleSubmit(e) {
-        e.preventDefault();
-        name === "" ? alert("Задача не может быть пустой!") : props.addTask(name)
-        setName("")
+    function handleChange(event) {
+        setNameInput(event.target.value)
     }
 
-    function handleChange(e) {
-        setName(e.target.value)
+    function handleSubmit(event) {
+        event.preventDefault();
+        nameInput === "" ? alert('Задача не может быть пустой') : props.addTask(nameInput);
     }
 
-    return(
+    return (
         <form onSubmit={handleSubmit}>
-            <h2 className="label-wrapper">
-                <label htmlFor="new-todo-input" className="label__lg">
-                    What needs to be done?
-                </label>
-            </h2>
-            <input
-                type="text"
-                id="new-todo-input"
-                className="input input__lg"
-                name="text"
-                autoComplete="off"
-                value={name}
-                onChange={handleChange}
-            />
-            <button type="submit" className="btn btn__primary btn__lg">
+        <h2 className="label-wrapper">
+            <label htmlFor="new-todo-input" className="label__lg">
+                What needs to be done?
+            </label>
+        </h2>
+        <input
+            type="text"
+            id="new-todo-input"
+            className="input input__lg"
+            name="text"
+            autoComplete="off"
+            value={nameInput}
+            onChange={handleChange}
+        />
+            <button type="submit" className="btn btn__primary btn__lg" >
                 Add
             </button>
         </form>
-    );
+    )
 }
 
-export default Form;
+export default Form; 
